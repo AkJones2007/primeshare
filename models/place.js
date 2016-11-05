2b6413c1c8a824983e3d41be73edbc8578e97133
Alex Jones <akjones2007@gmail.com>
2016-11-05 04:52:14 +0000

    Adds Place model

--- models/place.js
+++ models/place.js
@@ -2,13 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var placeSchema = Schema({
-  state: { type: String, required: true },
-  county: { type: String, required: true },
-  city: { type: String, required: true },
-  neighborhood: String,
-  latitude: { type: String, required: true },
-  longitude: { type: String, required: true },
-  name: String
+    address: { type: String, required: true },
+    state: { type: String, required: true },
+    county: { type: String, required: true },
+    city: { type: String, required: true },
+    neighborhood: String,
+    latitude: { type: String, required: true },
+    longitude: { type: String, required: true },
+    name: String
 });
 
 var Place = mongoose.model('Place', placeSchema);
