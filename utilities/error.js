fbd48a3949e7583c050ae53498897207133dab53
Alex <AkJones2007@gmail.com>
2016-11-05 01:49:44 -0400

    Adds utility for error handling

---
+++ utilities/error.js
@@ -0,0 +1,10 @@
+var error = {
+
+    unknown: {
+        message: 'An unknown error occured. Please try again later.',
+        dateOccured: new Date().toISOString()
+    }
+
+};
+
+module.exports = error;
